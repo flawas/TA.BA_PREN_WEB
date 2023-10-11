4f6f9dc8ec0ead6d0c8f0e040c95754f7698b20e
flawas <32436687+flawas@users.noreply.github.com>
2023-10-11 22:08:40 +0200

    Changes in header and footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,17 @@
 <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <div class="col-md-4 d-flex align-items-center">
+<div class="row">
+
+<div class="col-sm-4 md-4 mb-3">
     <a href="https://hslu.ch" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-        <img src="https://www.hslu.ch/-/media/campus/common/images/header/hslu-logo.svg" width="auto" height="30">
+        <img src="https://www.hslu.ch/-/media/campus/common/images/header/hslu-logo.svg" width="auto" height="60">
     </a>
-    <span class="mb-3 mb-md-0 text-body-secondary">© 2023 Team 33 PREN</span>
     </div>
+
+    <div class="col-sm-4 md-4 mb-3">
+        <p class="mb-3 mb-md-0 text-body-secondary">© 2023 PREN Team 33 </p>
+    </div>
+</div>
+
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
--- includes/head.php
+++ includes/head.php
@@ -7,19 +7,18 @@
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom sticky-top bg-light">
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <span class="fs-4">PREN Team 33</span>
-      </a>
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom sticky-top bg-light">
+            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                <span class="fs-4">PREN Team 33</span>
+            </a>
 
-      <ul class="nav nav-pills">
-        <li class="nav-item"><a href="index.php" class="nav-link" aria-current="page">Home</a></li>
-        <li class="nav-item"><a href="#team" class="nav-link" aria-current="page">Team</a></li>
-        <!--<li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">About</a></li>-->
-      </ul>
-    </header>
-  
+            <ul class="nav nav-pills">
+                <li class="nav-item"><a href="index.php" class="nav-link" aria-current="page">Home</a></li>
+                <li class="nav-item"><a href="#team" class="nav-link" aria-current="page">Team</a></li>
+                <!--<li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">About</a></li>-->
+            </ul>
+        </header>
 
     </head>
 
