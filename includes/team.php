b0ddbf910c053504b861861ee90162108c97719a
flawas <32436687+flawas@users.noreply.github.com>
2023-10-11 21:04:56 +0200

    Update team.php

--- includes/team.php
+++ includes/team.php
@@ -1,8 +1,8 @@
-<div class="container text-center">
+<div class="container text-center" id="team">
   <div class="row row-cols-1 row-cols-md-3">
+
   <div class="card ">
     <img src="includes/img/julia.jpeg" class="card-img-top" alt="Bild Julia">
-        <div class="card-header">Julia von Mühlenen</div>
         <div class="card-body">
           <h5 class="card-title">Julia von Mühlenen</h5>
           <p class="card-text">Projektleitung,</p>
@@ -11,113 +11,56 @@
     </div>
   </div>
 
+  <div class="card ">
+    <img src="includes/img/silvan.jpeg" class="card-img-top" alt="Bild Silvan">
+        <div class="card-body">
+          <h5 class="card-title">Silvan Mangold</h5>
+          <p class="card-text">Datenauswertung, Risiko,</p>
+          <p class="card-text">Kommunikation (Stv.)</p>
+      </div>
+    </div>
+  </div>
 
+  <div class="card ">
+    <img src="includes/img/flavio.jpeg" class="card-img-top" alt="Bild Flavio">
+        <div class="card-body">
+          <h5 class="card-title">Flavio Waser</h5>
+          <p class="card-text">Datenauswertung,</p>
+          <p class="card-text">Finanzen</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="card ">
+    <img src="includes/img/lukas.jpeg" class="card-img-top" alt="Bild Lukas">
+        <div class="card-body">
+          <h5 class="card-title">Lukas Agner</h5>
+          <p class="card-text">Elektrotechnik,</p>
+          <p class="card-text"></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="card ">
+    <img src="includes/img/richard.jpeg" class="card-img-top" alt="Bild Richard">
+        <div class="card-body">
+          <h5 class="card-title">Richard Schmassmann</h5>
+          <p class="card-text">Maschinenbau,</p>
+          <p class="card-text">Terminplanung, Kommunikation</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="card ">
+    <img src="includes/img/julia.jpeg" class="card-img-top" alt="Bild Julia">
+        <div class="card-body">
+          <h5 class="card-title">Alexaner Reinhard</h5>
+          <p class="card-text">Nachhaltigkeit</p>
+          <p class="card-text"></p>
+      </div>
+    </div>
+  </div>
 
 
   </div>
 </div>
-
-<!-- Team 1 - Bootstrap Brain Component -->
-<div class="col-sm-6 md-4 mb-3">
-
-
-<section class="bg-light py-3 py-md-5 py-xl-8" id="team">
-  <div class="container">
-    <div class="row justify-content-md-center">
-      <div class="col-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6">
-        <h2 class="mb-4 display-5 text-center">Team</h2>
-        <p class="text-secondary mb-5 text-center lead fs-4">Wir sind eine Gruppe von innovativen, erfahrenen und kompetenten Studenten. </p>
-        <hr class="w-50 mx-auto mb-5 mb-xl-9 border-dark-subtle">
-      </div>
-    </div>
-  </div>
-
-  <div class="container overflow-hidden">
-    <div class="row gy-4 gy-lg-0 gx-xxl-5">
-      <div class="col-12 col-md-6 col-lg-3">
-        <div class="card border-0 border-bottom border-primary shadow-sm overflow-hidden">
-          <div class="card-body p-0">
-            <figure class="m-0 p-0">
-              <img class="img-fluid" loading="lazy" src="includes/img/julia.jpeg" alt="">
-              <figcaption class="m-0 p-4">
-                <h4 class="mb-1">Julia von Mühlenen</h4>
-                <p class="text-secondary mb-0">Projektleitung,</p>
-                <p class="text-secondary mb-0">Datenauswertung</p>
-              </figcaption>
-            </figure>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-md-6 col-lg-3">
-        <div class="card border-0 border-bottom border-primary shadow-sm overflow-hidden">
-          <div class="card-body p-0">
-            <figure class="m-0 p-0">
-              <img class="img-fluid" loading="lazy" src="includes/img/silvan.jpeg" alt="">
-              <figcaption class="m-0 p-4">
-                <h4 class="mb-1">Silvan Mangold</h4>
-                <p class="text-secondary mb-0">Datenauswertung, Risiko,</p>
-                <p class="text-secondary mb-0">Kommunikation (Stv.)</p>
-              </figcaption>
-            </figure>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-md-6 col-lg-3">
-        <div class="card border-0 border-bottom border-primary shadow-sm overflow-hidden">
-          <div class="card-body p-0">
-            <figure class="m-0 p-0">
-              <img class="img-fluid" loading="lazy" src="includes/img/flavio.jpeg" alt="">
-              <figcaption class="m-0 p-4">
-                <h4 class="mb-1">Flavio Waser</h4>
-                <p class="text-secondary mb-0">Datenauswertung,</p>
-                <p class="text-secondary mb-0">Finanzen</p>
-              </figcaption>
-            </figure>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-md-6 col-lg-3">
-        <div class="card border-0 border-bottom border-primary shadow-sm overflow-hidden">
-          <div class="card-body p-0">
-            <figure class="m-0 p-0">
-              <img class="img-fluid" loading="lazy" src="includes/img/lukas.jpeg" alt="">
-              <figcaption class="m-0 p-4">
-                <h4 class="mb-1">Lukas Agner</h4>
-                <p class="text-secondary mb-0">Elektrotechnik</p>
-                <p class="text-secondary mb-0"></p>
-              </figcaption>
-            </figure>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-md-6 col-lg-3">
-        <div class="card border-0 border-bottom border-primary shadow-sm overflow-hidden">
-          <div class="card-body p-0">
-            <figure class="m-0 p-0">
-              <img class="img-fluid" loading="lazy" src="includes/img/richard.jpeg" alt="">
-              <figcaption class="m-0 p-4">
-                <h4 class="mb-1">Richard Schmassmann</h4>
-                <p class="text-secondary mb-0">Maschinenbau,</p>
-                <p class="text-secondary mb-0">Terminplanung, Kommunikation</p>
-              </figcaption>
-            </figure>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-md-6 col-lg-3">
-        <div class="card border-0 border-bottom border-primary shadow-sm overflow-hidden">
-          <div class="card-body p-0">
-            <figure class="m-0 p-0">
-              <img class="img-fluid" loading="lazy" src="includes/img/alexander.jpeg" alt="">
-              <figcaption class="m-0 p-4">
-                <h4 class="mb-1">Alexaner Reinhard</h4>
-                <p class="text-secondary mb-0">Nachhaltigkeit</p>
-                <p class="text-secondary mb-0"></p>
-              </figcaption>
-            </figure>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
