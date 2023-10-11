0c113ce04d0c37082647af4b233c45b4a01b66fc
flawas <32436687+flawas@users.noreply.github.com>
2023-10-11 21:01:03 +0200

    Update team.php

--- includes/team.php
+++ includes/team.php
@@ -1,10 +1,32 @@
+<div class="container text-center">
+  <div class="row row-cols-1 row-cols-md-3">
+  <div class="card ">
+    <img src="includes/img/julia.jpeg" class="card-img-top" alt="Bild Julia">
+        <div class="card-header">Julia von Mühlenen</div>
+        <div class="card-body">
+          <h5 class="card-title">Julia von Mühlenen</h5>
+          <p class="card-text">Projektleitung,</p>
+          <p class="card-text">Datenauswertung</p>
+      </div>
+    </div>
+  </div>
+
+
+
+
+  </div>
+</div>
+
 <!-- Team 1 - Bootstrap Brain Component -->
+<div class="col-sm-6 md-4 mb-3">
+
+
 <section class="bg-light py-3 py-md-5 py-xl-8" id="team">
   <div class="container">
     <div class="row justify-content-md-center">
       <div class="col-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6">
-        <h2 class="mb-4 display-5 text-center">Our Team</h2>
-        <p class="text-secondary mb-5 text-center lead fs-4">We are a group of innovative, experienced, and proficient teams. You will love to collaborate with us.</p>
+        <h2 class="mb-4 display-5 text-center">Team</h2>
+        <p class="text-secondary mb-5 text-center lead fs-4">Wir sind eine Gruppe von innovativen, erfahrenen und kompetenten Studenten. </p>
         <hr class="w-50 mx-auto mb-5 mb-xl-9 border-dark-subtle">
       </div>
     </div>
@@ -19,7 +41,8 @@
               <img class="img-fluid" loading="lazy" src="includes/img/julia.jpeg" alt="">
               <figcaption class="m-0 p-4">
                 <h4 class="mb-1">Julia von Mühlenen</h4>
-                <p class="text-secondary mb-0">Projektleitung, Datenauswertung</p>
+                <p class="text-secondary mb-0">Projektleitung,</p>
+                <p class="text-secondary mb-0">Datenauswertung</p>
               </figcaption>
             </figure>
           </div>
@@ -32,7 +55,8 @@
               <img class="img-fluid" loading="lazy" src="includes/img/silvan.jpeg" alt="">
               <figcaption class="m-0 p-4">
                 <h4 class="mb-1">Silvan Mangold</h4>
-                <p class="text-secondary mb-0">Datenauswertung, Risiko, Kommunikation (Stv.)</p>
+                <p class="text-secondary mb-0">Datenauswertung, Risiko,</p>
+                <p class="text-secondary mb-0">Kommunikation (Stv.)</p>
               </figcaption>
             </figure>
           </div>
@@ -45,7 +69,8 @@
               <img class="img-fluid" loading="lazy" src="includes/img/flavio.jpeg" alt="">
               <figcaption class="m-0 p-4">
                 <h4 class="mb-1">Flavio Waser</h4>
-                <p class="text-secondary mb-0">Datenauswertung, Finanzen</p>
+                <p class="text-secondary mb-0">Datenauswertung,</p>
+                <p class="text-secondary mb-0">Finanzen</p>
               </figcaption>
             </figure>
           </div>
@@ -59,6 +84,7 @@
               <figcaption class="m-0 p-4">
                 <h4 class="mb-1">Lukas Agner</h4>
                 <p class="text-secondary mb-0">Elektrotechnik</p>
+                <p class="text-secondary mb-0"></p>
               </figcaption>
             </figure>
           </div>
@@ -71,7 +97,8 @@
               <img class="img-fluid" loading="lazy" src="includes/img/richard.jpeg" alt="">
               <figcaption class="m-0 p-4">
                 <h4 class="mb-1">Richard Schmassmann</h4>
-                <p class="text-secondary mb-0">Maschinenbau, Terminplanung, Kommunikation</p>
+                <p class="text-secondary mb-0">Maschinenbau,</p>
+                <p class="text-secondary mb-0">Terminplanung, Kommunikation</p>
               </figcaption>
             </figure>
           </div>
@@ -85,6 +112,7 @@
               <figcaption class="m-0 p-4">
                 <h4 class="mb-1">Alexaner Reinhard</h4>
                 <p class="text-secondary mb-0">Nachhaltigkeit</p>
+                <p class="text-secondary mb-0"></p>
               </figcaption>
             </figure>
           </div>
