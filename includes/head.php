1c15244b6b6cdc37035519a373e8a52f6a40fe37
flawas <32436687+flawas@users.noreply.github.com>
2023-10-11 21:57:39 +0200

    Sticky navbar

--- includes/head.php
+++ includes/head.php
@@ -7,7 +7,7 @@
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom sticky-top bg-light">
       <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
         <span class="fs-4">PREN Team 33</span>
       </a>
