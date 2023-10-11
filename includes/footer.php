35d2b290a09e7d50166fe06cbea53c9cea772a2c
flawas <32436687+flawas@users.noreply.github.com>
2023-10-11 20:38:58 +0200

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+    <a href="https://hslu.ch" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+        <img src="https://www.hslu.ch/-/media/campus/common/images/header/hslu-logo.svg" width="auto" height="30">
+    </a>
+    <span class="mb-3 mb-md-0 text-body-secondary">© 2023 Team 33 PREN</span>
+    </div>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+<!--Closing Tag from head.php-->
+</div>
+
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once("includes/head.php");
+
+?>
+<html>
+    <body>
+
+
+    <div class="row">
+        <div class="col-md-12 themed-grid-col"><?php //do something ?></div>
+    </div>
+
+    </body>
+</html>
+
+<?php
+include_once("includes/footer.php");
+?>
