3a52467a8bd567ba2f341f7270c4af75849ffac5
flawas <32436687+flawas@users.noreply.github.com>
2023-10-11 21:19:35 +0200

    Update team.php

--- includes/team.php
+++ includes/team.php
@@ -1,71 +1,72 @@
 <div class="container text-center">
-  <div class="row">
-    <div class="col">
-        <div class="card">
-            <img src="includes/img/julia.jpeg" class="card-img-top" alt="Bild Julia">
-                <div class="card-body">
-                <h5 class="card-title">Julia von Mühlenen</h5>
-                <p class="card-text">Projektleitung,</p>
-                <p class="card-text">Datenauswertung</p>
-                </div>
-        </div>
-    </div>
-
-    <div class="col">
-        <div class="card">
-            <img src="includes/img/silvan.jpeg" class="card-img-top" alt="Bild Silvan">
-                <div class="card-body">
-                <h5 class="card-title">Silvan Mangold</h5>
-                <p class="card-text">Datenauswertung, Risiko,</p>
-                <p class="card-text">Kommunikation (Stv.)</p>
-                </div>
-        </div>
-    </div>
-
-    <div class="col">
-        <div class="card ">
-            <img src="includes/img/flavio.jpeg" class="card-img-top" alt="Bild Flavio">
-                <div class="card-body">
-                <h5 class="card-title">Flavio Waser</h5>
-                <p class="card-text">Datenauswertung,</p>
-                <p class="card-text">Finanzen</p>
-                </div>
-        </div>
-    </div>
-
-    <div class="col">
-        <div class="card">
-            <img src="includes/img/lukas.jpeg" class="card-img-top" alt="Bild Lukas">
-                <div class="card-body">
-                <h5 class="card-title">Lukas Agner</h5>
-                <p class="card-text">Elektrotechnik</p>
-                <p class="card-text"></p>
-            </div>
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <img src="includes/img/julia.jpeg" class="card-img-top" alt="Bild Julia">
+                    <div class="card-body">
+                    <h5 class="card-title">Julia von Mühlenen</h5>
+                    <p class="card-text">Projektleitung,</p>
+                    <p class="card-text">Datenauswertung</p>
+                    </div>
             </div>
         </div>
-    </div>
 
-    <div class="col">
-        <div class="card">
-            <img src="includes/img/richard.jpeg" class="card-img-top" alt="Bild Richard">
-                <div class="card-body">
-                <h5 class="card-title">Richard Schmassmann</h5>
-                <p class="card-text">Maschinenbau,</p>
-                <p class="card-text">Terminplanung, Kommunikation</p>
+        <div class="col">
+            <div class="card">
+                <img src="includes/img/silvan.jpeg" class="card-img-top" alt="Bild Silvan">
+                    <div class="card-body">
+                    <h5 class="card-title">Silvan Mangold</h5>
+                    <p class="card-text">Datenauswertung, Risiko,</p>
+                    <p class="card-text">Kommunikation (Stv.)</p>
+                    </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card ">
+                <img src="includes/img/flavio.jpeg" class="card-img-top" alt="Bild Flavio">
+                    <div class="card-body">
+                    <h5 class="card-title">Flavio Waser</h5>
+                    <p class="card-text">Datenauswertung,</p>
+                    <p class="card-text">Finanzen</p>
+                    </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card">
+                <img src="includes/img/lukas.jpeg" class="card-img-top" alt="Bild Lukas">
+                    <div class="card-body">
+                    <h5 class="card-title">Lukas Agner</h5>
+                    <p class="card-text">Elektrotechnik</p>
+                    <p class="card-text"></p>
                 </div>
+                </div>
+            </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <img src="includes/img/richard.jpeg" class="card-img-top" alt="Bild Richard">
+                    <div class="card-body">
+                    <h5 class="card-title">Richard Schmassmann</h5>
+                    <p class="card-text">Maschinenbau,</p>
+                    <p class="card-text">Terminplanung, Kommunikation</p>
+                    </div>
+            </div>
+        </div>
                 
-    <div class="col">
-        <div class="card">
-            <img src="includes/img/alexander.jpeg" class="card-img-top" alt="Bild Alexander">
-                <div class="card-body">
-                <h5 class="card-title">Alexaner Reinhard</h5>
-                <p class="card-text">Nachhaltigkeit</p>
-                <p class="card-text"></p>
-                </div>
+        <div class="col">
+            <div class="card">
+                <img src="includes/img/alexander.jpeg" class="card-img-top" alt="Bild Alexander">
+                    <div class="card-body">
+                    <h5 class="card-title">Alexaner Reinhard</h5>
+                    <p class="card-text">Nachhaltigkeit</p>
+                    <p class="card-text"></p>
+                    </div>
+            </div>
         </div>
-    </div>
 
-  </div>
+    </div>
 </div>
