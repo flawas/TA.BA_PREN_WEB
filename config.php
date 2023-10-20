ea7a01d50a440549b41a05546f176ade64504b64
flawas <32436687+flawas@users.noreply.github.com>
2023-10-20 12:45:27 +0200

    Script for data input into database

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('DATABASE_USERNAME', 'YOUR-USERNAME'); 
+define('DATABASE_PASSWORD', 'YOUR-PASSWORD');
+define('DATABASE_NAME', 'YOUR-DATABASE');
+define('DATABASE_HOST', 'localhost');
+define('WRITE_LOG', true); //Enable log.txt File
+?>
---
+++ upload-data.php
@@ -0,0 +1,22 @@
+<?php
+
+require 'config.php';
+
+$post = file('php://input');
+$data = json_decode($post[0]);
+
+$time = $data->time;
+$energy = $data->energy;
+
+$db_connect = mysqli_connect(DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME) or die(mysql_error());
+
+$server_datetime = date("Y-m-d H:i:s");
+
+mysqli_query($db_connect, "INSERT INTO 'pren_web' ('id', 'datetime', 'time', 'energy') "
+        . "VALUES (NULL, '$server_datetime', '$time', '$energy');");
+
+if (WRITE_LOG == true) {
+    file_put_contents('log.txt', $post[0] . PHP_EOL, FILE_APPEND);
+}
+
+?>
