e545a1d7cfbe394b7723164792742a4fc7a7ec3c
Flavio Waser <32436687+flawas@users.noreply.github.com>
2024-01-11 09:58:29 +0100

    Removed hslu logo from footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,10 +2,6 @@
 <div class="row">
 
 <div class="col-sm-4 md-4 mb-3">
-    <a href="https://hslu.ch" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-        <img src="https://www.hslu.ch/-/media/campus/common/images/header/hslu-logo.svg" width="auto" height="60">
-    </a>
-    </div>
 
     <div class="col-sm-4 md-4 mb-3">
         <p class="mb-3 mb-md-0 text-body-secondary">© 2023 PREN Team 33 </p>
