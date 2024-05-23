fe2933b8bfb7fee83c240bc8cc78056975f719a5
flawas <32436687+flawas@users.noreply.github.com>
2024-05-23 11:08:04 +0200

    Added pictures and videos

--- includes/videocards.php
+++ includes/videocards.php
@@ -3,6 +3,13 @@
   <br>
   <div class="row row-cols-1 row-cols-md-3">
 
+
+      <div class="card-body">
+          <h5 class="card-title">Titel</h5>
+              <video controls>
+                  <source src="https://www.youtube.com/shorts/TJPef3Fk7PY" type="video/mp4">
+              </video>
+      </div>
       <?php
       $dir = "./includes/testversuche/";
 
